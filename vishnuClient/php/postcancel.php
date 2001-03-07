e4f91152dc191c92a05de0190c9933474d3f4a6e
gvidaver <gvidaver>
2001-03-07 02:34:39 +0000

    updated

--- vishnuClient/php/postcancel.php
+++ vishnuClient/php/postcancel.php
@@ -1,14 +1,23 @@
 <?
   Header("Content-Type: text/plain");
-  require ("browserlink.php");
-/*  require ("utilities.php"); */
+//  require ("browserlink.php");
+ require ("utilities.php");
 
-//  if ($password == "nopassword")
-//    $password = "";
+  if ($password == "nopassword")
+    $password = "";
 
-//  $mysql_link = mysql_connect ("localhost",$username,$password);
+  $mysql_link = mysql_connect ("localhost",$username,$password);
   mysql_db_query ("vishnu_central",
                   "update scheduler_request set percent_complete=-1 " .
                   "where  problem = \"" . $problem . "\";");
+/*
+  $result = mysql_db_query ("vishnu_central",
+              "select percent_complete from scheduler_request where " .
+              "problem = \"" . $problem ."\" and number = -1;");
+  $value = mysql_fetch_row ($result);
+
+  echo "<message>Now " . $value . "</message>";
+*/
+  echo "OK";
   mysql_close();
 ?>
