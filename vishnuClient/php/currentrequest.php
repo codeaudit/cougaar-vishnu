6cf04bc65db11f51f84b60a1fc3e57da6b9e178e
dmontana <dmontana>
2001-04-03 19:24:53 +0000

    do locking to handle multiple schedulers

--- vishnuClient/php/currentrequest.php
+++ vishnuClient/php/currentrequest.php
@@ -5,41 +5,57 @@
   echo "<CURRENTREQUEST>\n";
 
   // user and password are global post variables
-  $mysql_link = mysql_connect ("localhost",$user,$password);
+  $mysql_link = mysql_connect ("localhost", $user, $password);
 
-  $result = mysql_db_query ("vishnu_central",
-                            "select * from scheduler_request where " .
-                            "percent_complete = 0 order by request_time;");
-  $earliest = 0;
-  if ($result == 0) {
+  $locklife = 8;
+  $t = time();
+  $d = makedate ($t);
+  $t2 = $t - $locklife;
+  mysql_db_query ("vishnu_central",
+      "delete from request_lock where unix_timestamp(lock_time) < $t2;");
+  if (mysql_errno()) {
     echo "<br>Error accessing database vishnu central.\n";
     echo "User : " . $user . "\n";
     echo "Pass : " . $password . "\n";
-    echo "Sql  : " . "select * from scheduler_request where " .
-         "'percent_complete = 0' order by request_time;\n"; 
   }
   else {
-    while ($value = mysql_fetch_array ($result)) {
-      if ($value["legal_hosts"]) {
-        $legalhosts = explode (",", $value["legal_hosts"]);
-        $found = 0;
-        while (list ($key, $host) = each ($legalhosts)) {
-          $host = trim ($host);
-          if ($host == substr ($localhost, 0, strlen ($host))) {
-            $found = 1;
-            break;
+    mysql_db_query ("vishnu_central",
+        "insert into request_lock values (\"$localhost\", \"$d\");");
+    if (! mysql_errno()) {
+      $result = mysql_db_query ("vishnu_central",
+                    "select lock_time from request_lock order by lock_time");
+      if ($result) {
+        $value = mysql_fetch_row ($result);
+        if ($value[0] != $d) {
+          mysql_db_query ("vishnu_central",
+                          "delete from request_lock where lock_time = \"$d\";");
+        }
+        else {
+          $result = mysql_db_query ("vishnu_central",
+                        "select * from scheduler_request where " .
+                        "percent_complete = 0 order by request_time;");
+          $earliest = 0;
+          while ($value = mysql_fetch_array ($result)) {
+            if ($value["legal_hosts"]) {
+              $legalhosts = explode (",", $value["legal_hosts"]);
+              $found = 0;
+              while (list ($key, $host) = each ($legalhosts)) {
+                $host = trim ($host);
+                if ($host == substr ($localhost, 0, strlen ($host))) {
+                  $found = 1;
+                  break;
+                }
+              }
+              if (! $found)
+                continue;
+            }
+            echo "<PROBLEM name=\"" . $value["problem"] . "\" number=\"" .
+                 $value["number"] . "\" />\n";
           }
         }
-        if (! $found)
-          continue;
       }
-      echo "<PROBLEM name=\"" . $value["problem"] . "\" number=\"" .
-           $value["number"] . "\" />\n";
     }
   }
-
-  mysql_free_result ($result);
-
   echo "</CURRENTREQUEST>\n";
   mysql_close();
 ?>
