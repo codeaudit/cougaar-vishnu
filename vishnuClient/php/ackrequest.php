1739676a1dc17fe6beef900b37dcab73e16fcbd2
dmontana <dmontana>
2001-04-04 15:39:04 +0000

    forgot to set max_traces to 20

--- vishnuClient/php/ackrequest.php
+++ vishnuClient/php/ackrequest.php
@@ -1,7 +1,20 @@
 <?
+  // ackrequest.php accepts acknowledgements and status updates from
+  // the schedulers.
+  //
+  // When a scheduler initially chooses to work on a problem, it
+  // passes in a percent_complete of 1.
+  // When it finishes, it sets percent_complete to be 100 and, if there
+  // was an error, sets message and trace to describe the error.
+  // For intermediate updates, a scheduler sets percent_complete to
+  // its estimate of that value.
+  //
+  // This returns the string "canceled" if the request has been
+  // canceled by a user, thus indicating to the scheduler to stop.
+
   require ("utilities.php");
 
-  $mysql_link = mysql_connect ("localhost",$user,$password);
+  $mysql_link = mysql_connect ("localhost", $user, $password);
   $result = mysql_db_query ("vishnu_central",
               "select percent_complete from scheduler_request where " .
               "problem = \"" . $problem ."\" and number = $number;");
@@ -19,7 +32,7 @@
           "insert into stack_traces values (\"$problem\", " .
           "\"$localhost\", \"" . makedate (time()) .
           "\", \"$message\", \"$trace\");");
-      $max_traces = 2;
+      $max_traces = 20;
       $result = mysql_db_query ("vishnu_central",
                     "select trace_time from stack_traces order by trace_time");
       $excess = mysql_num_rows ($result) - $max_traces;
