4c4c1069c8393f0e29d1ae38eab703707e1a106a
dmontana <dmontana>
2001-05-07 13:44:33 +0000

    forgot this

---
+++ vishnuClient/php/copyproblem.php
@@ -0,0 +1,67 @@
+<?
+  require ("browserlink.php");
+  require_once ("utilities.php");
+  require ("parseproblem.php");
+  require ("specssupport.php");
+  require ("gaparmssupport.php");
+  require ("datasupport.php");
+  require ("navigation.php");
+
+  function getTitle () {
+    global $problem, $newproblem;
+    if (! $newproblem)
+      echo "Copying problem $problem";
+    else
+      echo "Created problem $newproblem";
+  }
+
+  function getHeader() {
+    global $problem, $newproblem, $error, $user, $password;
+    if (! $newproblem) {
+      echo "Select name of new problem to which to copy " .
+           "<nobr>$problem</nobr>\n";
+      return;
+    }
+    if (! isNameLegal ($newproblem)) {
+      $error = 1;
+      echo "Error, illegal name for problem: <nobr>$newproblem</nobr>";
+      return;
+    }
+    ob_start();
+
+    echo "<PROBLEM name=\"" . $problem . "\" >\n";
+    writedataformat ($problem);
+    writeschedulingspecs ($problem);
+    writegaparms ($problem);
+    writedata ($problem);
+    echo "</PROBLEM>\n";
+
+    $str = ob_get_contents();
+    ob_end_clean();
+    parseproblem ($str, $newproblem);
+    echo "Created problem $newproblem";
+  }
+
+  function getSubheader() { 
+  }
+
+  function mainContent () {
+    global $problem, $newproblem, $error;
+    if ($error)
+      return;
+    if (! $newproblem) {
+?>
+<FORM method=get action="copyproblem.php">
+<INPUT type=text name=newproblem size=30>
+<INPUT type=hidden name=problem value="<? echo $problem; ?>">
+<INPUT type=submit value="Go">
+</FORM>
+<?
+      return;
+    }
+    echo "<br><br><DIV align=center><a href=\"problem.php?problem=" .
+         $newproblem . "\"/><font size=+2>Go to Problem " . $newproblem .
+         "</font></a></DIV>";
+
+  }
+?>
