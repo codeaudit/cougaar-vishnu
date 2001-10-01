3523514767a2a516d44ec9a7b7c3acc9fe872b57
gvidaver <gvidaver>
2001-10-01 21:48:02 +0000

    marker for result handlers

---
+++ vishnuClient/src/org/cougaar/lib/vishnu/client/ResultHandler.java
@@ -0,0 +1,8 @@
+package org.cougaar.lib.vishnu.client;
+
+/** 
+ * Marker interface for result handlers, either XML or Direct <p>
+ * Meant to work with ResultListeners.
+ */
+public interface ResultHandler {
+}
