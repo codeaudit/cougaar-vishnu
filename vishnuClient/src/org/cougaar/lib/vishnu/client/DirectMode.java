1e99cfb02cec3ec767c3dfa60b9ef98649856882
gvidaver <gvidaver>
2001-10-01 21:49:12 +0000

    moved parsing of results to DirectResultHandler

--- vishnuClient/src/org/cougaar/lib/vishnu/client/DirectMode.java
+++ vishnuClient/src/org/cougaar/lib/vishnu/client/DirectMode.java
@@ -18,14 +18,23 @@ import java.util.List;
 import java.util.Vector;
 import org.w3c.dom.Document;
 
+/** 
+ * Direct mode.  
+ * <p>
+ * Creates an internal instance of the scheduler and talks to it, instead
+ * of to a web server.  Uses direct translation of Cougaar to Vishnu objects,
+ * and reads the assignments directly.  <b>No XML involved.</b>
+ *
+ * Needs the plugin to be a DirectResultListener.  Mainly this means implementing
+ * the prepareVishnuObjects method.
+ * @see DirectResultListener
+ */
 public class DirectMode extends InternalMode {
   public DirectMode (ModeListener parent, VishnuComm comm, XMLProcessor xmlProcessor, 
-					 VishnuDomUtil domUtil, VishnuConfig config, 
+					 VishnuDomUtil domUtil, VishnuConfig config, ResultHandler resultHandler,
 					 ParamMap myParamTable) {
-	super (parent, comm, xmlProcessor, domUtil, config, 
-		   null /*direct mode doesn't need a result handler */, 
+	super (parent, comm, xmlProcessor, domUtil, config, resultHandler,
 		   myParamTable);
-	resultListener = (DirectResultListener) parent;
 	localSetup ();
   }
 	
@@ -37,6 +46,17 @@ public class DirectMode extends InternalMode {
 	catch(Exception e) {debugParseAnswer = false;}
   }
   
+  /**  
+	* Handles direct mode differently from external/internal. 
+	* <p> 
+	* Calls prepareVishnuObjects on listener to populate <tt>vishnuTasks</tt> and 
+	* <tt>vishnuResources</tt> lists with Vishnu objects.  
+	* <p>
+	* Also sends header with the time window information, and any other 
+	* data to scheduler via the internal buffer (in VishnuComm). 
+	* @param stuffToSend Cougaar tasks and assets
+	* @param objectFormatDoc used to figure out info about fields
+	*/
   public void prepareData (List stuffToSend, Document objectFormatDoc) {
 	vishnuTasks     = new ArrayList ();
 	vishnuResources = new ArrayList ();
@@ -44,9 +64,9 @@ public class DirectMode extends InternalMode {
 
 	Date start = new Date ();
 	   
-	resultListener.prepareVishnuObjects (stuffToSend, resultListener.getChangedAssets(), 
-										 vishnuTasks, vishnuResources, changedVishnuResources,
-										 objectFormatDoc, sched.getTimeOps());
+	((DirectModeListener)parent).prepareVishnuObjects (stuffToSend, parent.getChangedAssets(), 
+													   vishnuTasks, vishnuResources, changedVishnuResources,
+													   objectFormatDoc, sched.getTimeOps());
 	parent.clearChangedAssets ();
 
 	if (myExtraOutput)
@@ -68,9 +88,8 @@ public class DirectMode extends InternalMode {
    * Uses <tt>internalBuffer</tt> to send the specs, the object format and other data.<br>
    * Adds the task and resources (<tt>addTasksDirectly</tt>), and then runs the scheduler
    * (<tt>Scheduler.scheduleInternal</tt>).  Reads the assignments with 
-   * <tt>directlyHandleAssignments</tt>.  Any tasks not handled = any which are left in the
-   * <tt>myTaskUIDtoObject</tt> map get sent to <tt>handleImpossibleTasks</tt>.<p>
-   *
+   * <tt>directlyHandleAssignments</tt>. 
+   * <p>
    * <tt>vishnuTasks</tt> and <tt>vishnuResources</tt> are created in prepareData and populated
    * in #prepareVishnuObjects. <p>
    *
@@ -104,7 +123,7 @@ public class DirectMode extends InternalMode {
 							parent.getNumTasks () + " tasks in ", start2);
 
 	  // get the assignments the scheduler made, make plan elements
-	  directlyHandleAssignments (sched, data, timeOps);
+	  ((DirectResultHandler)resultHandler).directlyHandleAssignments (sched, data, timeOps);
 	} catch (Exception e) {
 	  System.out.println (getName () + ".runDirectly - Got error running scheduler : " + e.getMessage ());
       e.printStackTrace ();
@@ -112,6 +131,8 @@ public class DirectMode extends InternalMode {
 	  cleanUpAfterScheduling (unhandledTasks);
 	}
   }
+
+  /** calls methods on SchedulingData to add tasks, resources, and changed resources */
   protected void addTasksDirectly (SchedulingData data, List vishnuTasks, List vishnuResources,
 								   List changedVishnuResources) {
 	Date start = new Date ();
@@ -141,114 +162,8 @@ public class DirectMode extends InternalMode {
 	  }
   }
 
-   protected void directlyHandleAssignments (Scheduler sched, SchedulingData data,
-											 TimeOps timeOps) {
-	 Date start = new Date ();
-
-	 // This shows how to extract the assignments after scheduling.
-	 // When sched.assignmentsMultitask() is true, the assignments
-	 // will actually be MultitaskAssignment objects instead.
-	 if (! sched.assignmentsMultitask()) {
-	   org.cougaar.lib.vishnu.server.Task[] tasks = data.getTasks();
-	   for (int i = 0; i < tasks.length; i++) {
-		 Assignment assign = tasks[i].getAssignment();
-		 if (assign != null) {
-		   org.cougaar.lib.vishnu.server.Task task = assign.getTask();
-		   Resource resource = assign.getResource();
-		 
-		   parseAssignment (task.getKey(), 
-							resource.getKey(),
-							timeOps.timeToDate (assign.getTaskStartTime()),
-							timeOps.timeToDate (assign.getTaskEndTime()),
-							timeOps.timeToDate (assign.getStartTime()), // setup
-							timeOps.timeToDate (assign.getEndTime()));  // wrapup
-		 }
-		 else
-		   System.out.println (getName () + ".directlyHandleAssignments - no assignment for task " + 
-							   tasks[i].getKey() + "?");
-	   }
-    } else {
-      Resource[] resources = data.getResources();
-      for (int i = 0; i < resources.length; i++) {
-        MultitaskAssignment[] multi = resources[i].getMultitaskAssignments();
-		if (multi.length > 0) {
-		  if (myExtraOutput) 
-			System.out.println (getName () + " for resource " + resources[i].getKey() +
-								" got " + multi.length + " task groups assigned.");
-		  
-		  for (int j = 0; j < multi.length; j++) {
-			org.cougaar.lib.vishnu.server.Task [] multiTasks = multi[j].getTasks();
-			Vector tasks = new Vector ();
-			MultitaskAssignment assign = multi[j];
-			
-			for (int k = 0; k < multiTasks.length; k++) {
-			  Task task = getTaskFromAssignment(multiTasks[k].getKey());
-			  if (task != null)
-				tasks.add (task);
-			}
-
-			if (!tasks.isEmpty ()) {
-			  resultListener.handleMultiAssignment (tasks, 
-											getAssignedAsset   (assign.getResource().getKey()),
-											timeOps.timeToDate (assign.getTaskStartTime()),
-											timeOps.timeToDate (assign.getTaskEndTime()),
-											timeOps.timeToDate (assign.getStartTime()), // setup
-											timeOps.timeToDate (assign.getEndTime()));  // wrapup
-			}
-		  }
-		}
-	  }
-    } 
-
-	if (showTiming)
-	  domUtil.reportTime (".directlyHandleAssignments - parsed assignments in ", start);
-  }
-
-
-  /** called by runDirectly when the scheduler is finished for each assignment */
-  protected void parseAssignment (String task, String resource, Date assignedStart, Date assignedEnd, 
-								  Date assignedSetup, Date assignedWrapup) {
-	if (debugParseAnswer)
-	  System.out.println ("Assignment: "+
-						  "\ntask     = " + task +
-						  "\nresource = " + resource +
-						  "\nsetup    = " + assignedSetup +
-						  "\nstart    = " + assignedStart +
-						  "\nend      = " + assignedEnd +
-						  "\nwrapup   = " + assignedWrapup);
-
-	Task handledTask    = getTaskFromAssignment (task);
-	Asset assignedAsset = getAssignedAsset (resource);
-
-	if (handledTask != null)
-	  resultListener.handleAssignment (handledTask, assignedAsset, assignedStart, assignedEnd, assignedSetup, assignedWrapup);
-  }
-  
-  protected Task getTaskFromAssignment (String task) {
-	StringKey taskKey = new StringKey (task);
-	Task handledTask  = resultListener.getTaskForKey (taskKey);
-	if (handledTask == null) {
-	  if (myExtraOutput)
-		System.err.println (getName () + ".getTaskFromAssignment - NOTE - no ALP task found for task key " + taskKey);
-	  return null;
-	}
-	else {
-	  resultListener.removeTask (taskKey);
-	}
-	
-	return handledTask;
-  }
-  
-  protected Asset getAssignedAsset (String resource) {
-	Asset assignedAsset = resultListener.getAssetForKey (new StringKey (resource));
-	if (assignedAsset == null) 
-	  System.out.println (getName() + ".parseMultiAssignment - ERROR - no asset found with " + resource);
-	return assignedAsset;
-  }
-
-  protected String getName () { return "DirectMode"; }
+  protected String getName () { return parent.getName () + "-DirectMode"; }
 
   protected List vishnuTasks, vishnuResources, changedVishnuResources;
   protected boolean debugParseAnswer = false;
-  DirectResultListener resultListener;
 }
