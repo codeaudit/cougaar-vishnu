3842c5b2f0d6dcf56b68b712662f7560154ab5e5
gvidaver <gvidaver>
2001-10-01 21:51:04 +0000

    added comments, removed clearTasks, not a ModeListener anymore

--- vishnuClient/src/org/cougaar/lib/vishnu/client/ResultListener.java
+++ vishnuClient/src/org/cougaar/lib/vishnu/client/ResultListener.java
@@ -1,5 +1,6 @@
 package org.cougaar.lib.vishnu.client;
 
+import java.util.Collection;
 import java.util.Date;
 import java.util.Vector;
 
@@ -7,17 +8,31 @@ import org.cougaar.util.StringKey;
 import org.cougaar.domain.planning.ldm.plan.Task;
 import org.cougaar.domain.planning.ldm.asset.Asset;
 
-public interface ResultListener extends ModeListener {
+/** What the plugin must do to handle scheduler results */
+public interface ResultListener {
   // task stuff
-  void clearTasks ();
+  /** 
+   * Used to look up Cougaar task when result returned by Vishnu 
+   */ 
   Task getTaskForKey (StringKey key);
   void removeTask (StringKey key);
 
   // asset stuff
   int getNumAssets ();
   Asset getAssetForKey (StringKey key);
+  /** which assets were changed since they were added as new assets? */
+  Collection getChangedAssets ();
 
+  /** 
+   * Called for each Vishnu assignment that needs to be translated into a Cougaar plan element 
+   * This is for one-to-one assignments : allocations
+   */
   void handleAssignment (Task task, Asset asset, Date start, Date end, Date setupStart, Date wrapupEnd);
+
+  /**
+   * Called for each Vishnu assignment that needs to be translated into a Cougaar plan element 
+   * This is for many-to-one assignments : aggregations.
+   */
   void handleMultiAssignment (Vector tasks, Asset asset, Date start, Date end, Date setupStart, Date wrapupEnd);
 
   // misc
