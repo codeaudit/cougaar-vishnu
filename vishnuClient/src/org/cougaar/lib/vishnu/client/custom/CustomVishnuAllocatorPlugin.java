019179e325aad8b2c6acc23901bd39b2af725f84
gvidaver <gvidaver>
2007-02-09 18:00:23 +0000

    Removed dependence on GLM.

--- vishnuClient/src/org/cougaar/lib/vishnu/client/custom/CustomVishnuAllocatorPlugin.java
+++ vishnuClient/src/org/cougaar/lib/vishnu/client/custom/CustomVishnuAllocatorPlugin.java
@@ -25,47 +25,44 @@
  */
 package org.cougaar.lib.vishnu.client.custom;
 
+import com.bbn.vishnu.scheduling.SchedulingData;
+import org.cougaar.lib.vishnu.client.VishnuAllocatorPlugin;
+import org.cougaar.lib.vishnu.client.XMLProcessor;
+import org.cougaar.lib.vishnu.client.XMLizer;
+import org.cougaar.planning.ldm.asset.Asset;
+import org.w3c.dom.Document;
+
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
-
-import org.cougaar.glm.ldm.asset.GLMAsset;
-import org.cougaar.planning.ldm.asset.Asset;
-
-import org.cougaar.lib.vishnu.client.VishnuAllocatorPlugin;
-import org.cougaar.lib.vishnu.client.XMLizer;
-import org.cougaar.lib.vishnu.client.XMLProcessor;
-import com.bbn.vishnu.scheduling.SchedulingData;
-
-import org.w3c.dom.Document;
 
 /**
  * Uses CustomDataXMLize to create Vishnu objects either directly or through XML.
  */
 public class CustomVishnuAllocatorPlugin extends VishnuAllocatorPlugin {
 
-  /** 
+  /**
    * Overrides VishnuPlugin.createXMLProcessor                           <p>
    *
    * Use a different data xmlizer to create the data xml stream to send to vishnu,<br>
    * specifically, TranscomDataXMLize.
-   * 
+   *
    */
-  protected XMLProcessor createXMLProcessor  () { 
+  protected XMLProcessor createXMLProcessor  () {
     if (isInfoEnabled())
       debug (getName () + ".createXMLProcessor - creating TRANSCOM xml processor.");
 
     return new XMLProcessor (getMyParams(), getName(), getClusterName(), domUtil, comm, getConfigFinder(), logger, logger, logger) {
-	public void createDataXMLizer (Map nameToDescrip, String assetClassName) {
-	  if (isInfoEnabled())
-	    info (this.getName() + ".createDataXMLizer - setting data xmlizer.");
-		  
-	  setDataXMLizer(createXMLizer(getRunDirectly()));
-	}
-      }; 
+      public void createDataXMLizer (Map nameToDescrip, String assetClassName) {
+        if (isInfoEnabled())
+          info (this.getName() + ".createDataXMLizer - setting data xmlizer.");
+
+        setDataXMLizer(createXMLizer(getRunDirectly()));
+      }
+    };
   }
 
-  /** 
+  /**
    * Create a CustomDataXMLize XMLizer 
    * @see CustomDataXMLize
    */
@@ -73,34 +70,33 @@ public class CustomVishnuAllocatorPlugin extends VishnuAllocatorPlugin {
     return new CustomDataXMLize (direct, logger);
   }
 
-  /** 
+  /**
    * Creates lists of Vishnu objects.  
    *
-   * @param tasksAndResources - Cougaar tasks and resources to translate
+   * @param alpObjects - Cougaar tasks and resources to translate
    * @param vishnuTasks - list to add Vishnu tasks to 
    * @param vishnuResources - list to add Vishnu resources to 
-   * @param objectFormat - contains field type info necessary to create fields on Vishnu objects
+   * @param formatDoc - contains field type info necessary to create fields on Vishnu objects
    * @param schedData - scheduling data object used when making Vishnu dates
    */
-  public void prepareVishnuObjects (List alpObjects, Collection changed, 
-				    List vishnuTasks, List vishnuResources, 
-				    List changedVishnuResources,
-				    Document formatDoc, SchedulingData schedData) { 
+  public void prepareVishnuObjects (List alpObjects, Collection changed,
+                                    List vishnuTasks, List vishnuResources,
+                                    List changedVishnuResources,
+                                    Document formatDoc, SchedulingData schedData) {
     DirectTranslator dt = (DirectTranslator) getDataXMLizer ();
     ((CustomDataXMLize) dt).setFormatDoc (formatDoc, schedData);
     dt.createVishnuObjects (alpObjects, changed, vishnuTasks, vishnuResources, changedVishnuResources);
   }
 
-  /** 
+  /**
    * Highest confidence if physical asset, medium confidence if an organization.<p>
-   * 
+   *
    * Used by handleAssignment to figure out confidence of allocation. <p>
    *
    * @see org.cougaar.lib.vishnu.client.VishnuAllocatorPlugin#handleAssignment
    * @param asset to test whether physical asset -- its doing the work in the allocation
    */
   protected double getConfidence (Asset asset) {
-    return ((GLMAsset) asset).hasPhysicalPG () ? 
-      allocHelper.HIGHEST_CONFIDENCE : allocHelper.MEDIUM_CONFIDENCE;
+    return allocHelper.HIGHEST_CONFIDENCE;
   }
 }
